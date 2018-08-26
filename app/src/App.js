f30837b9edabf76e667b83557de73734fa578deb
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-08-26 17:09:13 +0300

    Initial food app.

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Food app</div>
+    );
+  }
+}
+
+export default App;
