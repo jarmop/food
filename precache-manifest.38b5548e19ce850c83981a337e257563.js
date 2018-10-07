3c9ad9cf22241daa9c02b330052e9917593a3e3c
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-10-07 17:17:12 +0300

    Updates

---
+++ precache-manifest.38b5548e19ce850c83981a337e257563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "67fe68913997935f0b4c",
+    "url": "/food/static/js/runtime~main.67fe6891.js"
+  },
+  {
+    "revision": "cfcaafa7a8d101ead446",
+    "url": "/food/static/js/main.cfcaafa7.chunk.js"
+  },
+  {
+    "revision": "5c58f859adb2d920ca30",
+    "url": "/food/static/js/1.5c58f859.chunk.js"
+  },
+  {
+    "revision": "cfcaafa7a8d101ead446",
+    "url": "/food/static/css/main.f04ab288.chunk.css"
+  },
+  {
+    "revision": "5c58f859adb2d920ca30",
+    "url": "/food/static/css/1.5c7cfa4c.chunk.css"
+  },
+  {
+    "revision": "27a6848f85f893b84faf28102e274551",
+    "url": "/food/index.html"
+  }
+];
