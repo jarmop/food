fed3f7a3bc2e7950f5b439db10f0ab6967f53168
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2019-12-01 14:11:36 +0200

    Updates

---
+++ precache-manifest.8f0d88f7e339870ebc18caadb75c3dc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc530783ff8611f2a07a6e6bd99d6afc",
+    "url": "/food/index.html"
+  },
+  {
+    "revision": "2be5a787bb40cc3a5019",
+    "url": "/food/static/css/2.a07c46d3.chunk.css"
+  },
+  {
+    "revision": "08c91784fefcbe567865",
+    "url": "/food/static/css/main.5c3e543b.chunk.css"
+  },
+  {
+    "revision": "2be5a787bb40cc3a5019",
+    "url": "/food/static/js/2.ef29149e.chunk.js"
+  },
+  {
+    "revision": "08c91784fefcbe567865",
+    "url": "/food/static/js/main.8b7112e5.chunk.js"
+  },
+  {
+    "revision": "f35a5b7048eec5ec4265",
+    "url": "/food/static/js/runtime-main.d5e4668e.js"
+  }
+]);
