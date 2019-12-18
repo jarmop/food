f436875d160bb311eea0ead513d80b76471ecc0f
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2019-12-18 20:48:24 +0200

    Refactor.

--- server/firestore.js
+++ server/firestore.js
@@ -1,21 +1,21 @@
-let firebase = require('firebase');
-let config = require('../config');
+const firebase = require('firebase');
+const config = require('../config');
 
 const COLLECTION_FOODS = 'foods';
 const COLLECTION_MEALS = 'meals';
 
 firebase.initializeApp(config.firebase);
 
-let db = firebase.firestore();
+const db = firebase.firestore();
 
 /**
  * @param foods
  * @returns {Promise<void>}
  */
 exports.setFoods = (foods) => {
-  let batch = db.batch();
+  const batch = db.batch();
   foods.forEach(food => {
-    let foodRef = db.collection(COLLECTION_FOODS).doc(food.id);
+    const foodRef = db.collection(COLLECTION_FOODS).doc(food.id);
     batch.set(foodRef, food);
   });
 
@@ -28,11 +28,10 @@ exports.setFoods = (foods) => {
 exports.getFoods = () => {
   return new Promise((resolve, reject) => {
     db.collection(COLLECTION_FOODS).get().then(collection => {
-      let foods = {};
-      collection.forEach(doc => {
-        let food = doc.data();
+      const foods = collection.reduce((foods, doc) => {
+        const food = doc.data();
         foods[food.id] = food;
-      });
+      }, {});
 
       resolve(foods);
     })
@@ -48,9 +47,9 @@ exports.setFood = (food) => {
 };
 
 exports.setMeals = (meals) => {
-  let batch = db.batch();
+  const batch = db.batch();
   meals.forEach(meal => {
-    let mealRef = db.collection(COLLECTION_MEALS).doc();
+    const mealRef = db.collection(COLLECTION_MEALS).doc();
     batch.set(mealRef, {foods: meal});
   });
 
